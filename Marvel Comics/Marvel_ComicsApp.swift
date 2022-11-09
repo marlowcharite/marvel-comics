144234223e85203827698394860d0062613b4ad2
Marlow Charite <marlowcharite@gmail.com>
2022-11-09 07:29:02 -0500

    Initial Commit

---
+++ Marvel Comics/Marvel_ComicsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Marvel_ComicsApp.swift
+//  Marvel Comics
+//
+//  Created by Marlow Charite on 11/9/22.
+//
+
+import SwiftUI
+
+@main
+struct Marvel_ComicsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Marvel ComicsUITests/Marvel_ComicsUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Marvel_ComicsUITestsLaunchTests.swift
+//  Marvel ComicsUITests
+//
+//  Created by Marlow Charite on 11/9/22.
+//
+
+import XCTest
+
+final class Marvel_ComicsUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
