c6aeaeb07c5b63c999c935ebd1a9f67abc7f2a24
Marlow Charite <marlowcharite@gmail.com>
2022-11-09 11:49:42 -0500

    create Secrets to read api key

---
+++ Marvel Comics/Secrets.swift
@@ -0,0 +1,35 @@
+//
+//  Secrets.swift
+//  Marvel Comics
+//
+//  Created by Marlow Charite on 11/9/22.
+//
+
+import Foundation
+
+final class Secrets {
+    
+    // MARK: - Properties
+    
+    var apiKey: String? {
+        guard let token = bundle.infoDictionary?[Key.publicKey.rawValue] as? String else { return nil }
+        return token
+    }
+    
+    private let bundle: Bundle
+    
+    // MARK: - Initializers
+    
+    /// Creaates new app secrets for a given bundle.
+    ///
+    /// - Parameter bundle: The bundle to read secrets from.
+    init(with bundle: Bundle = .main) {
+        self.bundle = bundle
+    }
+    
+    // MARK: - Supporting types
+    
+    private enum Key: String {
+        case publicKey = "API_KEY"
+    }
+}
